b71188c5201ca3efe4a4a79293da57d90d4d0eeb
Caurs <peteris.caurs@accenture.com>
2020-10-26 14:02:02 +0200

    word finder implementation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'normalize.css';
 import './App.css';
 
+import scrabbleWordFinder from './utilities/scrabbleWordFinder'
+
 function App() {
+  const [letterInput, setLetterInput] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  findWords = () => {
+    const results = scrabbleWordFinder.find(letterInput);
+    setSearchResults(results);
+  }
   return (
     <div className="app">
       <h1>SCRABBLE</h1>
       <p>vārdu meklētājs</p>
-      <input type="text" placeholder="raksti savus burtus šeit" />
-      <button>meklēt</button>
+      <input type="text" placeholder="raksti savus burtus šeit" onChange={e => setLetterInput(e.event.target)} />
+      <button onClick={findWords}>meklēt</button>
       <table style={{ margin: '2em auto 2em auto' }}>
         <thead>
           <th>vārds</th>
---
+++ src/utilities/scrabbleWordFinder.js
@@ -0,0 +1,50 @@
+import '../scrabbleWordList';
+
+class ScrabbleTreeNode {
+    constructor() {
+        this.children = Object.create(null);
+    }
+}
+
+class ScrabbleDictionary {
+    constructor(words) {
+        this.root = new ScrabbleTreeNode();
+        words.forEach(word => this.insert(word));
+    }
+    insert(word) {
+        let cursor = this.root;
+        for (let letter of word) {
+            if (!cursor.children[letter]) {
+                cursor.children[letter] = new ScrabbleTreeNode();
+            }
+            cursor = cursor.children[letter];
+        }
+        cursor.isWord = true;
+    }
+}
+
+class ScrabbleWordFinder {
+    constructor(dict) {
+        this.dict = new ScrabbleDictionary(dict)
+    }
+    find(letters) {
+        return this.validWords(this.dict.root, letters);
+    }
+    validWords = function (node, letters, word = '', results = []) {
+        if (node.isWord) {
+            results.push(word);
+        }
+        const seen = new Set();
+        for (let ch of letters) {
+            if (!seen.has(ch)) {
+                seen.add(ch);
+                if (node.children[ch]) {
+                    this.validWords(node.children[ch], letters.replace(ch, ''), word + ch, results);
+                }
+            }
+        }
+        return results;
+    };
+}
+
+export default new ScrabbleWordFinder(scrabbleWordList);
